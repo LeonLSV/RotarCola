a82635338a05bb8ac66f00cd07611d6fac01c6e5
Leon Ramiro <racramiro@gmail.com>
2021-02-06 19:34:37 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useRef, useState } from "react";
+import { Button } from "antd";
 
-function App() {
+const App = () => {
+  const valorInput = useRef(null);
+  const names = ["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"];
+  const [who, setWho] = useState("El último en tomar fue:");
+
+  const whoIsNext = (names, i) => {
+    if (i <= names.length) {
+      return setWho(names[i - 1]);
+    } else {
+      while (i > names.length) {
+        let primero = names.shift();
+        names.push(primero, primero);
+      }
+
+      return setWho(names[i - 1]);
+    }
+  };
+
+  const handleClick = () => {
+    const valor = valorInput.current.value;
+    console.log(whoIsNext(names, valor));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="App-header">
+      <h1 className="title">¿Quién sigue en la fila despues de:</h1>
+      <input type="number" ref={valorInput} />
+      <Button style={{ margin: "20px" }} type="primary" onClick={handleClick}>
+        Calcular
+      </Button>
+
+      <h1 className="title">{who}</h1>
+    </header>
   );
-}
+};
 
 export default App;
